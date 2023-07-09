7dfe506c0aa4228cb37bf181eca25e068b11340b
Konstantinos Tsolakidis <kontsolakidis25@gmail.com>
2023-07-09 15:50:18 +0300

    To releaseNewe

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const cors = require("cors");
 const app = express();
 app.use(bodyParser.json());
 app.use(compression());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(helmet({ crossOriginResourcePolicy: false }));
 
 const PORT = process.env.PORT || 3000;
